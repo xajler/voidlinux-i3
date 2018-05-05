2528d9634749e29a74289aee0d06002bc743e298
Kornelije Sajler <xajler@gmail.com>
2018-05-05 19:26:40 +0200

    Forgot to add vl-mklive.sh

---
+++ vl-mklive.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo bash mklive.sh -k dvorak -p 'vim bash bash-completion git gnupg gnupg2 yadm
+dialog cryptsetup lvm2 curl bootchart2 busybox dmraid' -b 'base-system neovim
+bash-completion neofetch tlp powertop htop lm_sensors fzf intel-ucode alsa-utils
+alsa-plugins alsa-lib alsa-firmware pulseaudio ConsoleKit2 pavucontrol xorg-server
+xorg-apps xorg-minimal xinit xcape xf86-video-intel xf86-input-libinput ranger
+qutebrowser calcurse mpd mpc mpv compton youtube-dl ffmpeg feh scrot tmux
+lxappearance xautolock mupdf cmatrix openvpn terminus-font ncmpcpp i3-gaps i3status
+i3lock i3lock acpi tree ImageMagick w3m w3m-img sxiv polybar xcalib bind-utils
+gnumeric R newsboat xclip noto-fonts-ttf firefox wget curl urlview base-devel
+fontconfig-devel libX11-devel libXft-devel libXinerama-devel freetype-devel
+acpi_call-dkms smartmontools gnupg gnupg2 yadm pass'
